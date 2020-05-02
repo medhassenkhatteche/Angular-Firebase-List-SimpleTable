e4ec0de19b144689be547db087997add3c4f7404
Mohamed Hassen Khatteche <63561361+medhassenkhatteche@users.noreply.github.com>
2020-05-02 10:22:41 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,13 +11,13 @@ export class AppComponent {
   constructor() {
     // Your web app's Firebase configuration
     var firebaseConfig = {
-      apiKey: "---",
-      authDomain: "---",
-      databaseURL: "---",
-      projectId: "---",
-      storageBucket: "---",
-      messagingSenderId: "---",
-      appId: "---"
+      apiKey: "AIzaSyDrEoDdGE9wmugx5Rb6NntObZwXeMZzQVU",
+      authDomain: "projetbib-e3082.firebaseapp.com",
+      databaseURL: "https://projetbib-e3082.firebaseio.com",
+      projectId: "projetbib-e3082",
+      storageBucket: "projetbib-e3082.appspot.com",
+      messagingSenderId: "599940306734",
+      appId: "1:599940306734:web:6afa17d9f36245edf84cbf"
     };
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
